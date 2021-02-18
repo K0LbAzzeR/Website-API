4ccca0ab4242a39b82b1167ad5318398a9a336a3
Justin Ndwiga <ndwigajustin@gmail.com>
2021-02-18 20:26:36 +0300

    mock email sending,add threading

---
+++ v1/utils/threading.py
@@ -0,0 +1,20 @@
+import threading
+
+
+class EmailThread(threading.Thread):
+    """To improve speed of sending emails
+
+    thread class enables us to send
+        emails on a separate process thread
+    """
+
+    def __init__(self, server, from_email, to_email, message):
+        """constructor"""
+        self.server = server
+        self.from_email = from_email
+        self.to_email = to_email
+        self.message = message
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.server.sendmail(self.from_email, [self.to_email], self.message)
